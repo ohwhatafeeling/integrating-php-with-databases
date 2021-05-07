57f03efe5092733e72da85d7437c7bd1b2a59947
ohwhatafeeling <pyro_michael@hotmail.com>
2021-05-08 04:14:38 +1000

    Add pagination links

--- inc/functions.php
+++ inc/functions.php
@@ -1,10 +1,108 @@
 <?php
 
-function full_catalog_array() {
+function get_catalog_count($category = null) {
+  $category = strtolower($category);
+  include ("connection.php");
+
+  try {
+    $sql = "SELECT COUNT(media_id) FROM Media";
+    if (!empty($category)) {
+      $results = $db->prepare(
+        $sql . " WHERE LOWER(category) = ?"
+      );
+      $results->bindParam(1, $category, PDO::PARAM_STR);
+    } else {
+      $results = $db->prepare($sql);
+    }
+    $results->execute();
+  } catch (Exception $e) {
+    echo "Bad Query";
+  }
+
+  $count = $results->fetchColumn(0);
+  return $count;
+}
+
+function full_catalog_array($limit = null, $offset = 0) {
+  include ("connection.php");
+
+  try {
+    $sql = "SELECT media_id, title, category, img
+    FROM Media
+    ORDER BY
+    REPLACE(
+      REPLACE(
+        REPLACE(title, 'The ', ''),
+        'An ',
+        ''
+      ),
+      'A ',
+      ''
+    )";
+    if (is_integer($limit)) {
+      $results = $db->prepare($sql . " LIMIT = ? OFFSET = ?");
+      $results->bindParam(1, $limit, PDO::PARAM_INT);
+      $results->bindParam(2, $offset, PDO::PARAM_INT);
+    } else {
+      $results = $db->prepare($sql);
+    }
+    $results->execute();
+  } catch(Exception $e) {
+    echo "Unable to retrive results";
+    exit;
+  }
+
+  $catalog = $results->fetchAll(PDO::FETCH_ASSOC);
+  return $catalog;
+}
+
+function category_catalog_array($category, $limit = null, $offset = 0) {
+  include "connection.php";
+  $category = strtolower($category);
+
+  try {
+    $sql = "SELECT media_id, title, category, img
+    FROM Media
+    WHERE LOWER(category) = ?
+    ORDER BY
+    REPLACE(
+      REPLACE(
+        REPLACE(title, 'The ', ''),
+        'An ',
+        ''
+      ),
+      'A ',
+      ''
+    )";
+    if (is_integer($limit)) {
+      $results = $db->prepare($sql . " LIMIT ? OFFSET ?");
+      $results->bindParam(1, $category, PDO::PARAM_STR);
+      $results->bindParam(2, $limit, PDO::PARAM_INT);
+      $results->bindParam(3, $offset, PDO::PARAM_INT);
+    } else {
+      $results = $db->prepare($sql);
+      $results->bindParam(1, $category, PDO::PARAM_STR);
+    }
+    $results->execute();
+  } catch(Exception $e) {
+    echo "Unable to retrive results";
+    exit;
+  }
+
+  $catalog = $results->fetchAll(PDO::FETCH_ASSOC);
+  return $catalog;
+}
+
+function random_catalog_array() {
   include "connection.php";
 
   try {
-    $results = $db->query("SELECT media_id, title, category, img FROM Media");
+    $results = $db->query(
+      "SELECT media_id, title, category, img
+      FROM Media
+      ORDER BY RANDOM()
+      LIMIT 4"
+    );
   } catch(Exception $e) {
     echo "Unable to retrive results";
     exit;
@@ -56,7 +154,7 @@ function single_item_array($id) {
   return $item;
 }
 
-function get_item_html($id,$item) {
+function get_item_html($item) {
     $output = "<li><a href='details.php?id="
         . $item["media_id"] . "'><img src='"
         . $item["img"] . "' alt='"
